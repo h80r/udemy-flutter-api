4e1d6a7ebec9d2339772ba81acc128879c64502c
h80r <h80rgbc@gmail.com>
2021-10-10 17:11:54 -0300

    Lesson 17 - Main Page UI I

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,6 +8,38 @@ class MainPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ScopedReader watch) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [_backgroundWidget()],
+        ),
+      ),
+    );
+  }
+
+  Widget _backgroundWidget() {
+    return Container(
+      constraints: const BoxConstraints.expand(),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        image: const DecorationImage(
+          image: NetworkImage(
+            'https://images-na.ssl-images-amazon.com/images/I/91B32iU7ayL._AC_SL1500_.jpg',
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(0.2),
+          ),
+        ),
+      ),
+    );
   }
 }
